660601522b10d431b300c43c1923e98a6d4e509f
Dmitriy Galnykin <d.galnykin@iconto.net>
2014-12-15 18:57:33 +0300

    db connected

--- public_html/classes/my_db.php
+++ public_html/classes/my_db.php
@@ -1,45 +1,49 @@
 <?php
-  class CMyDB
-  {
+class CMyDB
+{
     // переменные
     var $link = null;
     var $result = null;
     // ----------
 
-        var $case = null, $select = null, $where = null,
+    var $case = null, $select = null, $where = null,
         $order = null, $group = null, $limit = null, $join_table = null;
 
     // подключение к БД с заданными параметрами
-    function CMyDB($host = "", $db, $user="", $psw = "")
+    function CMyDB($host = "127.0.0.1", $db = "u565477573_kryo", $user = "u565477573_therm", $psw = "1992rhbjnthv")
     {
-      // подключение к MySQL
-      $this->link = mysql_connect($host, $user, $psw, 1)
-          or die("Could not connect : " . mysql_error($this->link));
+        // подключение к MySQL
+        $this->link = mysql_connect($host, $user, $psw)
+        or die("Could not connect : " . mysql_error($this->link));
 
-      // выбор БД
-      mysql_select_db($db, $this->link) or die("Could not select database");
+        // выбор БД
+        mysql_select_db($db, $this->link) or die("Could not select database");
+        mysql_query('SET NAMES cp1251');
+        mysql_query('SET CHARACTER SET cp1251');
+        mysql_query('SET COLLATION_CONNECTION=cp1251_general_ci');
     }
+
 
     // выполнение запроса
     function Execute($sql)
     {
-      $this->result = mysql_query($sql, $this->link)
-          or die("Query failed : " . mysql_error($this->link));
+        $this->result = mysql_query($sql, $this->link)
+        or die("Query failed : " . mysql_error($this->link));
 
-      // проверка результатов запроса $sql
-      if(!$this->result)
-        return null;
-      else
-        return $this->result;
+        // проверка результатов запроса $sql
+        if(!$this->result)
+            return null;
+        else
+            return $this->result;
     }
 
     // возвращает кол-во строк результата
     function NumRows($result = "")
     {
-      if($result != "")
-        return mysql_num_rows($result);
-      else
-        return mysql_num_rows($this->result);
+        if($result != "")
+            return mysql_num_rows($result);
+        else
+            return mysql_num_rows($this->result);
 
     }
 
@@ -69,18 +73,18 @@
         $this->resset_vars();
     }
 
-/*  Вставка в базу
-*/
+    /*  Вставка в базу
+	*/
     public function insert($data, $case = null)
     {
         $this->total_resset($case);
         $this->Execute("INSERT INTO ".TBL_PREF.$this->case." SET ".$this->get_set($data));
 
-    return $this->insert_id();
+        return $this->insert_id();
     }
 
-/*  Апдейт
-*/
+    /*  Апдейт
+	*/
     public function update($data, $where = null, $case = null)
     {
         $this->total_resset($case);
@@ -89,8 +93,8 @@
 
     }
 
-/*  Удаление
-*/
+    /*  Удаление
+	*/
     public function del($where = null, $case = null)
     {
         $this->total_resset($case);
@@ -98,22 +102,22 @@
                        ".(!empty($where)?"WHERE ".$this->get_where($where):""));
     }
 
-/*  Проверка на существование
-*/
+    /*  Проверка на существование
+	*/
     public function exist($data, $case = null)
     {
         $this->total_resset($case);
         $this->where = $this->get_where($data);
         $this->sql();
 
-    return $this->NumRows();
+        return $this->NumRows();
     }
 
-/*  Получить массив с данными
-    $case - таблица с данными
-    $where - условия для выборки (массив)
-    $operator - оператор сравнения для условия
-*/
+    /*  Получить массив с данными
+		$case - таблица с данными
+		$where - условия для выборки (массив)
+		$operator - оператор сравнения для условия
+	*/
     public function get_data($where = null, $case = null, $operator = '=', $ch1 = '', $ch2 = '')
     {
         $result = null;                  //print $ch1;
@@ -125,13 +129,13 @@
 
         while ($r = mysql_fetch_assoc($q)) $result[] = $r;
 
-    return $result;
+        return $result;
     }
 
-/*  Возвращает строку `имя поля`='значение',`имя поля`='значение', ... для вставки\апдейта
-    $operator - операторы сравнения ( =,>,< )
-    $ch1, $ch2 - спецсимволы для выражений типа LIKE('$поиск$')
-*/
+    /*  Возвращает строку `имя поля`='значение',`имя поля`='значение', ... для вставки\апдейта
+		$operator - операторы сравнения ( =,>,< )
+		$ch1, $ch2 - спецсимволы для выражений типа LIKE('$поиск$')
+	*/
     protected function get_set($array, $operator = '=', $ch1 = '"', $ch2 = '"')
     {
         $set = null;
@@ -144,27 +148,27 @@
                 $set .= ", ".$k." ".$operator." ".$ch1.trim($v).$ch2."";
         }
 
-    return substr($set,2);
+        return substr($set,2);
     }
 
-/*  Возвращает строку `имя поля`='значение' AND `имя поля`='значение' AND ...
-    $operator - операторы сравнения ( =,>,< )
-    $ch1, $ch2 - спецсимволы для выражений типа LIKE('$поиск$')
-*/
+    /*  Возвращает строку `имя поля`='значение' AND `имя поля`='значение' AND ...
+		$operator - операторы сравнения ( =,>,< )
+		$ch1, $ch2 - спецсимволы для выражений типа LIKE('$поиск$')
+	*/
     protected function get_where($array, $operator = '=', $ch1 = '', $ch2 = '')
     {
-    return str_replace(","," AND ", $this->get_set($array, $operator, $ch1, $ch2));
+        return str_replace(","," AND ", $this->get_set($array, $operator, $ch1, $ch2));
     }
 
-/*  Возвращает строку 'значение','значение'...
-*/
+    /*  Возвращает строку 'значение','значение'...
+	*/
     protected function get_select($array)
     {
-    return implode(',', $array);
+        return implode(',', $array);
     }
 
-/*  Менеджер sql запросов
-*/
+    /*  Менеджер sql запросов
+	*/
     protected function sql()
     {
         $sql = "SELECT ".(!empty($this->select)?$this->select:"*")."
@@ -176,14 +180,14 @@
                ".(!empty($this->limit)?"LIMIT ".$this->limit:"");
         $this->resset_vars();
         //print($sql);
-    return $this->Execute($sql);
+        return $this->Execute($sql);
     }
 
 
     // очитска памяти
     function Free()
     {
-      if($this->link != null) mysql_close($this->link);
+        if($this->link != null) mysql_close($this->link);
     }
-  }
+}
 ?>
