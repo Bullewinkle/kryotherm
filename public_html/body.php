95d66bca8697c45666d9a7f70b6fc177afd2a03c
Bullwinkle <developer085@gmail.com>
2014-12-21 20:45:04 +0300

    body formatted, improvements

--- public_html/body.php
+++ public_html/body.php
@@ -1,112 +1,101 @@
 <?
-  $show_right = null;
+$show_right = null;
 
-  if (!empty($_REQUEST['idCat']) ||
-      strpos($_SERVER['REQUEST_URI'],'cart.php') ||
-      !empty($_REQUEST['exec_order']) ||
-      strpos($_SERVER['REQUEST_URI'],'compare.php') ||
-      strpos($_SERVER['REQUEST_URI'],'filter_result.php')
-      )
+if (!empty($_REQUEST['idCat']) ||
+	strpos($_SERVER['REQUEST_URI'], 'cart.php') ||
+	!empty($_REQUEST['exec_order']) ||
+	strpos($_SERVER['REQUEST_URI'], 'compare.php') ||
+	strpos($_SERVER['REQUEST_URI'], 'filter_result.php')
+) {
+	$show_right = 1;
+}
 
-      $show_right = 1;
 
-  if(!empty($_REQUEST['newsId']))
-  {
-      if($_REQUEST['newsId'] == 'all') $r = getNews(3);   // архив
-      else                             $r = getNews(1); // конкретная новость
+if (!empty($_REQUEST['newsId'])) {
+	if ($_REQUEST['newsId'] == 'all') $r = getNews(3);   // архив
+	else                             $r = getNews(1); // конкретная новость
 
-          $txt = html_entity_decode($r['content']);
-          $desc = $r['description'];
-          $keyw = $r['keywords'];
-          $title = $r['title'];
+	$txt = html_entity_decode($r['content']);
+	$desc = $r['description'];
+	$keyw = $r['keywords'];
+	$title = $r['title'];
 
-      $txt .= getGallery($auth,1,'news');  // список прикрепленных к материалу галерей
+	$txt .= getGallery($auth, 1, 'news');  // список прикрепленных к материалу галерей
 
-  }
-  elseif(!empty($_REQUEST['galleryId'])){
+} elseif (!empty($_REQUEST['galleryId'])) {
 
-     $r = getGallery($auth,2);
-     $txt = html_entity_decode($r['content']);
-     $keyw = $r['keyw'];
-     $desc = $r['desc'];
-     $title = $r['title'];
+	$r = getGallery($auth, 2);
+	$txt = html_entity_decode($r['content']);
+	$keyw = $r['keyw'];
+	$desc = $r['desc'];
+	$title = $r['title'];
 
-  }
-  elseif(!empty($_REQUEST['imageId'])){
+} elseif (!empty($_REQUEST['imageId'])) {
 
-     $r = getGallery($auth,3);
+	$r = getGallery($auth, 3);
 
-     $txt = html_entity_decode($r['content']);
-     $keyw = $r['keyw'];
-     $desc = $r['desc'];
-     $title = $r['title'];
+	$txt = html_entity_decode($r['content']);
+	$keyw = $r['keyw'];
+	$desc = $r['desc'];
+	$title = $r['title'];
 
-  }
-  elseif($_SERVER['REQUEST_URI'] == '/gallery.php'){
+} elseif ($_SERVER['REQUEST_URI'] == '/gallery.php') {
 
-          $txt .= getGallery($auth,4,'all');
-  }
-  elseif(!empty($_REQUEST['idProd'])){
+	$txt .= getGallery($auth, 4, 'all');
+} elseif (!empty($_REQUEST['idProd'])) {
 
-      $r = getCatalog($auth,3,'',$_REQUEST['idCat'],$_REQUEST['idProd']);
-      $txt = html_entity_decode($r['content']);
-      $keyw = $r['keyw'];
-      $desc = $r['desc'];
-      $title = $r['title'];
+	$r = getCatalog($auth, 3, '', $_REQUEST['idCat'], $_REQUEST['idProd']);
+	$txt = html_entity_decode($r['content']);
+	$keyw = $r['keyw'];
+	$desc = $r['desc'];
+	$title = $r['title'];
 
-      $txt .= getGallery($auth,1,'product');
-  }
-  elseif(($_SERVER['REQUEST_URI'] == '/' or isset($_REQUEST['idManuf']) or isset($_REQUEST['idCat'])) and empty($_REQUEST['idProd'])){
+	$txt .= getGallery($auth, 1, 'product');
+} elseif (($_SERVER['REQUEST_URI'] == '/' or isset($_REQUEST['idManuf']) or isset($_REQUEST['idCat'])) and empty($_REQUEST['idProd'])) {
 
-     
-     if ($_SERVER['REQUEST_URI'] == '/')
 
-         $r = getCatalog($auth,4);
-     else
-         $r = getCatalog($auth,5, '', '', '', $filter);
+	if ($_SERVER['REQUEST_URI'] == '/')
 
-     $txt = html_entity_decode($r['content']);
-     $keyw = $r['keyw'];
-     $desc = $r['desc'];
-     $title = $r['title'];
-  }
-  elseif (strpos($_SERVER['REQUEST_URI'],'cart.php') || !empty($_REQUEST['exec_order']))
-  {
-      if (!empty($_REQUEST['clear_cart']))
-          $_SESSION['user_cart'] = array();
+		$r = getCatalog($auth, 4);
+	else
+		$r = getCatalog($auth, 5, '', '', '', $filter);
 
-      if ($_REQUEST['exec_order'] == 'form')
-      {
-          $txt .= get_order_form($_SESSION['user_cart'],(!empty($_REQUEST['customer'])?$_REQUEST['customer']:1));
-      }
-      elseif ($_REQUEST['exec_order'] == 'send')
-      {
-          $txt .= exec_order_form($mail, $_POST, $_SESSION['user_cart']);
-      }
-      else
-          $txt .= get_user_cart($_SESSION['user_cart']);
+	$txt = html_entity_decode($r['content']);
+	$keyw = $r['keyw'];
+	$desc = $r['desc'];
+	$title = $r['title'];
+} elseif (strpos($_SERVER['REQUEST_URI'], 'cart.php') || !empty($_REQUEST['exec_order'])) {
+	if (!empty($_REQUEST['clear_cart']))
+		$_SESSION['user_cart'] = array();
 
-  }elseif (strpos($_SERVER['REQUEST_URI'],'filter_result.php')){
+	if ($_REQUEST['exec_order'] == 'form') {
+		$txt .= get_order_form($_SESSION['user_cart'], (!empty($_REQUEST['customer']) ? $_REQUEST['customer'] : 1));
+	} elseif ($_REQUEST['exec_order'] == 'send') {
+		$txt .= exec_order_form($mail, $_POST, $_SESSION['user_cart']);
+	} else
+		$txt .= get_user_cart($_SESSION['user_cart']);
 
-      include(CATALOG_SCRIPT_DIR.'filter_result.php');
+} elseif (strpos($_SERVER['REQUEST_URI'], 'filter_result.php')) {
 
-  }elseif (strpos($_SERVER['REQUEST_URI'],'compare.php')){
+	include(CATALOG_SCRIPT_DIR . 'filter_result.php');
 
-      include(CATALOG_SCRIPT_DIR.'compare.php');
+} elseif (strpos($_SERVER['REQUEST_URI'], 'compare.php')) {
 
-  }elseif (strpos($_SERVER['REQUEST_URI'],'search.php')){
+	include(CATALOG_SCRIPT_DIR . 'compare.php');
 
-      include('searching.php');
+} elseif (strpos($_SERVER['REQUEST_URI'], 'search.php')) {
 
-  }else{
+	include('searching.php');
 
-    list($txt, $keyw, $desc, $title) =
-      PageView($auth, intval( $_REQUEST['page_id']),
-               ((!empty($_REQUEST['word']))? mysql_real_escape_string($_REQUEST['word']):""), mysql_real_escape_string($_SERVER['REQUEST_URI']) );
+} else {
 
-    $txt .= getGallery($auth,1,'pages');  // список прикрепленных к материалу галерей
+	list($txt, $keyw, $desc, $title) =
+		PageView($auth, intval($_REQUEST['page_id']),
+			((!empty($_REQUEST['word'])) ? mysql_real_escape_string($_REQUEST['word']) : ""), mysql_real_escape_string($_SERVER['REQUEST_URI']));
 
-    $copyright = "&copy; Сайт создан в <br>kreazone.ru '2009";
+	$txt .= getGallery($auth, 1, 'pages');  // список прикрепленных к материалу галерей
 
-  }
+	$copyright = "&copy; Сайт создан в <br>kreazone.ru '2009";
+
+}
 ?>
